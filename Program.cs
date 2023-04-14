b09d6eca3b13604573afac13155375443c517d1d
Rodrigo <rodrisaha6053@gmail.com>
2023-04-14 09:26:37 +0200

    inicio de la actividad

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Introduzca un número");
+String primerNumero;
+primerNumero = Console.ReadLine();
+Console.WriteLine("Introduzca otro número");
+string segundoNumero;
+segundoNumero = Console.ReadLine();
+int x = Int32.Parse(primerNumero);
+int y = Int32.Parse(segundoNumero);
+if (x > y)
+{
+Console.WriteLine(primerNumero + " es mayor que " + segundoNumero);
+}
