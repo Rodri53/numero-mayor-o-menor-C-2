f64ace065cb00e4d4b55421806927ec2f320e6c5
Rodrigo <rodrisaha6053@gmail.com>
2023-04-18 12:20:38 +0200

    tarea terminada

--- program.cs
+++ program.cs
@@ -1,8 +1,29 @@
-Console.WriteLine("Introduzca un número");
+﻿Console.WriteLine("Introduzca un número");
 String primerNumero;
 primerNumero = Console.ReadLine();
 Console.WriteLine("Introduzca otro número");
 string segundoNumero;
 segundoNumero = Console.ReadLine();
-int x = Int32.Parse(primerNumero);
-int y = Int32.Parse(segundoNumero);
+int primerNumeroEntero = Int32.Parse(primerNumero);
+int segundoNumeroEntero = Int32.Parse(segundoNumero);
+if (primerNumeroEntero > segundoNumeroEntero)
+{
+    Console.WriteLine(primerNumero + " es mayor que " + segundoNumero);
+}
+else if (primerNumeroEntero < segundoNumeroEntero)
+{
+    Console.WriteLine(primerNumero + " es menor que " + segundoNumero);
+}
+else
+{
+    Console.WriteLine(primerNumero + " es igual que " + segundoNumero);
+}
+
+if (segundoNumeroEntero % 2 == 0)
+{
+    Console.WriteLine("El número " + segundoNumero + " es un número par");
+}
+else
+{
+    Console.WriteLine("El número " + segundoNumero + " es un número impar");
+}
